f21fc6bf8014f47838a04356315b100d5e57d232
imranpanjwani <imranpanjwani00@gmail.com>
2018-04-15 01:35:31 +0530

    setup basic configuration to start the app

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-import express from "express";
+/**
+ * Module dependencies
+ */
+const express = require('express');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const chalk = require('chalk');
+
+/**
+ * Load environment variables from .env files ( API keys and passwords)
+ */
+dotenv.load({path: '.env'});
+
+/**
+ * Create Express App
+ */
+const app = express();
+
+/**
+ * Express configuration
+ */
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev')); 
+
+/**
+ * Start express server
+ */
+app.listen(app.get('port'), () => {
+    console.log('%s App is running on http://localhost:%d in %s mode.', chalk.green('\u2713'), app.get('port'), app.get('env'));
+})
