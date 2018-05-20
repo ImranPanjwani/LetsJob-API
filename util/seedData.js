a409365856459890d061c3f92bf2196028784000
imranpanjwani <imranpanjwani00@gmail.com>
2018-05-20 15:17:52 +0530

    update structure of job object

--- util/seedData.js
+++ util/seedData.js
@@ -7,50 +7,99 @@ module.exports = () => {
       title: 'Job 1',
       description: 'Description about job 1',
       category: 'ENGR',
-      contact_emails: ['abc@gmail.com'],
-      contact_numbers: ['9999999999']
+      contactEmails: ['abc@gmail.com'],
+      contactNumbers: ['9999999999'],
+      experience: [2, 4],
+      location: ['Hyderabad', 'Pune'],
+      salary: {
+        salaryRange: [200000],
+        salaryType: 'pa'
+      },
+      role: 'Senior Engineer'
     },
     {
       title: 'Job 2',
       description: 'Description about job 2',
       category: 'MARKT',
-      contact_emails: ['def@gmail.com'],
-      contact_numbers: ['8888888888']
+      contactEmails: ['def@gmail.com'],
+      contactNumbers: ['8888888888'],
+      experience: [1],
+      location: [],
+      salary: {
+        salaryRange: [200000, 350000],
+        salaryType: 'pa'
+      },
+      role: 'Business Development Manager'
     },
     {
-      title: 'Job 3',
-      description: 'Description about job 3',
+      title: 'A very very very long title for Job 3 to test the view in app',
+      description: 'Some test description about job 3 that might not be much interesting but will help the developer',
       category: 'TEACH',
-      contact_emails: ['ghi@gmail.com'],
-      contact_numbers: ['7777777777']
+      contactEmails: ['ghi@gmail.com'],
+      contactNumbers: ['7777777777'],
+      experience: [0],
+      location: ['Mumbai', 'Ahmedabad'],
+      salary: {
+        salaryRange: [10000, 15000],
+        salaryType: 'pm'
+      },
+      role: null
     },
     {
       title: 'Job 4',
       description: 'Description about job 4',
       category: 'IT',
-      contact_emails: ['lmn@gmail.com'],
-      contact_numbers: ['9999999999']
+      contactEmails: ['lmn@gmail.com'],
+      contactNumbers: ['9999999999'],
+      experience: [0, 3],
+      location: ['Jaipur', 'Chennai'],
+      salary: {
+        salaryRange: [400000, 800000],
+        salaryType: 'pa'
+      },
+      role: 'Software Developer'
     },
     {
       title: 'Job 5',
       description: 'Description about job 5',
       category: 'FINACC',
-      contact_emails: ['opq@gmail.com'],
-      contact_numbers: ['8888888888']
+      contactEmails: ['opq@gmail.com'],
+      contactNumbers: ['8888888888'],
+      experience: [2, 5],
+      location: ['Delhi', 'Hyderabad'],
+      salary: {
+        salaryRange: [12000, 18000],
+        salaryType: 'pm'
+      },
+      role: 'Accountant'
     },
     {
       title: 'Job 6',
       description: 'Description about job 6',
       category: 'BD',
-      contact_emails: ['rst@gmail.com'],
-      contact_numbers: ['7777777777']
+      contactEmails: ['rst@gmail.com'],
+      contactNumbers: ['7777777777'],
+      experience: [],
+      location: ['Mumbai', 'Hyderabad'],
+      salary: {
+        salaryRange: [120000, 250000],
+        salaryType: 'pa'
+      },
+      role: null
     },
     {
       title: 'Job 7',
       description: 'Description about job 7',
       category: 'BANK',
-      contact_emails: ['uvw@gmail.com'],
-      contact_numbers: ['9999999999']
+      contactEmails: ['uvw@gmail.com'],
+      contactNumbers: ['9999999999'],
+      experience: [1, 3],
+      location: ['Hyderabad', 'Nasik'],
+      salary: {
+        salaryRange: [20000, 25000],
+        salaryType: 'pm'
+      },
+      role: 'Banker'
     }
   ];
   Job.find({}).then((totalResults) => {
